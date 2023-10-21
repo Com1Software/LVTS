6feda6d845471752e27c2947445624063fac9b7f
Com1 Software, Inc <information@com1software.com>
2023-10-21 14:50:05 -0400

    Update main.ino

--- INO/Sensors with LED Display/main.ino
+++ INO/Sensors with LED Display/main.ino
@@ -1,19 +1,25 @@
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin = 2;
+const int echoPin = 3;
 LiquidCrystal_I2C lcd(0x27,16,2);
 long duration;
 int distance;
+Servo myservo1; 
 int position;
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
+  myservo1.attach(4);
   Serial.begin(9600); 
   lcd.init();
   lcd.backlight();
   }
 void loop() {
+  position++;
+  if (position > 120) position = 0;
+   myservo1.write(position);
    digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -24,13 +30,17 @@ void loop() {
   lcd.setCursor(0,0);
   lcd.print("Distance");
   lcd.setCursor(11,0);
+  lcd.print("     ");
+  lcd.setCursor(11,0);
   lcd.print(distance);
   lcd.setCursor(0,1);
   lcd.print("Position");
+  lcd.setCursor(11,1);
+  lcd.print("     ");
   lcd.setCursor(11,1);
   lcd.print(position);
   Serial.print("DIS=");
   Serial.println(distance);
   Serial.print("POS=");
-  Serial.println(distance);
+  Serial.println(position);
 }
