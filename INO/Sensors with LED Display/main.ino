ce6acb0987b5fec96a397ab9cefa8e32474486b0
Com1 Software, Inc <information@com1software.com>
2023-10-21 12:17:36 -0400

    Update main.ino

--- INO/Sensors with LED Display/main.ino
+++ INO/Sensors with LED Display/main.ino
@@ -6,7 +6,7 @@ int distance;
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
-  Serial.begin(115200); 
+  Serial.begin(9600); 
 }
 void loop() {
    digitalWrite(trigPin, LOW);
