6c767fbbdb8d6ed9e8030d9deb48e321e4b71924
Com1 Software, Inc <information@com1software.com>
2023-11-01 11:52:24 -0400

    Create sensor.ino

---
+++ INO/Serial Tests/sensor.ino
@@ -0,0 +1,13 @@
+void setup() {
+   Serial.begin(9600);
+   
+}
+
+void loop() {
+   Serial.print("DIS=");
+  Serial.println("0000");
+  Serial.print("POS=");
+  Serial.println("0000");
+  Serial.print("\n\r");
+  
+}
