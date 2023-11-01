70937956655b38d1cdc2fc8b0b55ba1b02ec515a
Com1 Software, Inc <information@com1software.com>
2023-11-01 10:05:59 -0400

    Create controller.ino

---
+++ INO/Serial Tests/controller.ino
@@ -0,0 +1,13 @@
+void setup() {
+   Serial.begin(9600);
+   
+}
+
+void loop() {
+  Serial.print("CH1=");
+  Serial.print(",CH2=");
+  Serial.print(",CH3=");
+  Serial.print(",CH4=");
+  Serial.print("\n\r");
+  
+}
