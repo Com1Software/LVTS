34f501025cd92e17a398e9bfb56f16b3b9cb30d9
Dave <information@com1software.com>
2023-11-05 10:48:30 -0500

    update

--- Go/Pi Test Vehicle/main.go
+++ Go/Pi Test Vehicle/main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"runtime"
 	"strings"
+	"time"
 
 	"go.bug.st/serial"
 )
 
+// -------------------------------------------------------------------------
 func main() {
-	fmt.Println("Pi Test Vehicle")
-	gpsport := ""
-	rcport := ""
-	senport := ""
-	gtg := 3
+	agent := SSE()
+	xip := fmt.Sprintf("%s", GetOutboundIP())
+	port := "8080"
+	//	s1 := rand.NewSource(time.Now().UnixNano())
+	//	r1 := rand.New(s1)
+	//
+	//--- tctl 0 = normal mode test
+	//         1 = high speed mode test
+	//
+	tctl := 1
+	tc := 0
+	fmt.Println("Test SSE Server")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	fmt.Printf("Outbound IP  : %s Port : %s\n", xip, port)
+	if runtime.GOOS == "windows" {
+		xip = "http://localhost"
+	}
 	ports, err := serial.GetPortsList()
 	if err != nil {
 		log.Fatal(err)
@@ -30,156 +47,124 @@ func main() {
 		DataBits: 8,
 		StopBits: serial.OneStopBit,
 	}
-	dp := fmt.Sprintf("Detected %d Ports", len(ports))
-	fmt.Println(dp)
 	for x := 0; x < len(ports); x++ {
-		if ports[x][8:11] == "ACM" {
-			port, err := serial.Open(ports[x], mode)
-			if err != nil {
-				log.Fatal(err)
-			}
-			dp := fmt.Sprintf("Detecting Port %d", x+1)
-			fmt.Println(dp)
-			line := ""
-			buff := make([]byte, 1)
+		port, err := serial.Open(ports[x], mode)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go func() {
 			for {
-				n, err := port.Read(buff)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if n == 0 {
-					port.Close()
-					break
-				}
-				line = line + string(buff[:n])
-				if strings.Contains(string(buff[:n]), "\n") {
-					port.Close()
-					break
-				}
-
-			}
-			if len(line) > 3 {
 				switch {
-				case line[0:3] == "$GP":
-					gpsport = ports[x]
-				case line[0:3] == "CH1":
-					rcport = ports[x]
-				case line[0:3] == "DIS":
-					senport = ports[x]
+				case tctl == 0:
+					time.Sleep(time.Second * 1)
+				case tctl == 1:
+					time.Sleep(time.Second * -1)
+					tc++
+				}
+				line := ""
+				buff := make([]byte, 1)
+				for {
+					n, err := port.Read(buff)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if n == 0 {
+						port.Close()
+						fmt.Println("closed Port")
+						break
+					}
 
+					if strings.Contains(string(buff[:n]), "\n") {
+						break
+					} else {
+						line = line + string(buff[:n])
+					}
 				}
 
+				agent.Notifier <- []byte(line)
+				fmt.Println(line)
+			}
+		}()
+	}
+
+	fmt.Printf("Listening at  : %s Port : %s\n", xip, port)
+	if runtime.GOOS == "windows" {
+		http.ListenAndServe(":"+port, agent)
+	} else {
+		http.ListenAndServe(xip+":"+port, agent)
+	}
+}
+
+type Agent struct {
+	Notifier    chan []byte
+	newuser     chan chan []byte
+	closinguser chan chan []byte
+	user        map[chan []byte]bool
+}
+
+func SSE() (agent *Agent) {
+	agent = &Agent{
+		Notifier:    make(chan []byte, 1),
+		newuser:     make(chan chan []byte),
+		closinguser: make(chan chan []byte),
+		user:        make(map[chan []byte]bool),
+	}
+	go agent.listen()
+	return
+}
+
+func (agent *Agent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	flusher, ok := rw.(http.Flusher)
+	if !ok {
+		http.Error(rw, "Error ", http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	mChan := make(chan []byte)
+	agent.newuser <- mChan
+	defer func() {
+		agent.closinguser <- mChan
+	}()
+	notify := req.Context().Done()
+	go func() {
+		<-notify
+		agent.closinguser <- mChan
+	}()
+	for {
+		fmt.Fprintf(rw, "%s", <-mChan)
+		flusher.Flush()
+	}
+
+}
+
+func (agent *Agent) listen() {
+	for {
+		select {
+		case s := <-agent.newuser:
+			agent.user[s] = true
+		case s := <-agent.closinguser:
+			delete(agent.user, s)
+		case event := <-agent.Notifier:
+			for userMChan, _ := range agent.user {
+				userMChan <- event
 			}
 		}
-
 	}
 
-	if len(gpsport) > 0 {
-		fmt.Printf("GPS Port %s\n", gpsport)
-		gtg--
-	} else {
-		fmt.Printf("GPS Port Not Found\n")
+}
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
 	}
-	if len(rcport) > 0 {
-		fmt.Printf("RC Port %s\n", rcport)
-		gtg--
-	} else {
-		fmt.Printf("RC Port Not Found\n")
-	}
-	if len(senport) > 0 {
-		fmt.Printf("Sensor Port %s\n", senport)
-		gtg--
-	} else {
-		fmt.Printf("Sensor Port Not Found\n")
-	}
-	if gtg == 0 {
-		fmt.Printf("Good to go\n")
-		fmt.Printf("All ports found\n")
-		porta, err := serial.Open(gpsport, mode)
-		if err != nil {
-			log.Fatal(err)
-		}
-		portb, err := serial.Open(senport, mode)
-		if err != nil {
-			log.Fatal(err)
-		}
-		portc, err := serial.Open(rcport, mode)
-		if err != nil {
-			log.Fatal(err)
-		}
+	defer conn.Close()
 
-		for {
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-			linea := ""
-			lineb := ""
-			linec := ""
-			la := true
-			lb := true
-			lc := true
-			buff := make([]byte, 1)
-			on := true
-			for on != false {
-				linea = ""
-				lineb = ""
-				linec = ""
-				if la {
-					for {
-						n, err := porta.Read(buff)
-						if err != nil {
-							log.Fatal(err)
-						}
-						if n == 0 {
-							fmt.Println("\nEOF")
-							break
-						}
-						linea = linea + string(buff[:n])
-						if strings.Contains(string(buff[:n]), "\n") {
-							break
-						}
-
-					}
-				}
-				if lb {
-					for {
-						n, err := portb.Read(buff)
-						if err != nil {
-							log.Fatal(err)
-						}
-						if n == 0 {
-							fmt.Println("\nEOF")
-							break
-						}
-						lineb = lineb + string(buff[:n])
-						if strings.Contains(string(buff[:n]), "\n") {
-							break
-						}
-
-					}
-				}
-				if lc {
-					for {
-						n, err := portc.Read(buff)
-						if err != nil {
-							log.Fatal(err)
-						}
-						if n == 0 {
-							fmt.Println("\nEOF")
-							break
-						}
-						linec = linec + string(buff[:n])
-						if strings.Contains(string(buff[:n]), "\n") {
-							break
-						}
-
-					}
-				}
-
-				fmt.Printf("%s  - %s - %s \n", linea[0:18], lineb, linec)
-
-			}
-		}
-
-	} else {
-		fmt.Printf("Init Failure  %d ports not found\n", gtg)
-	}
+	return localAddr.IP
 }
