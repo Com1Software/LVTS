a40495e73faffcf8f5283927020ee03e4a47b2ac
Dave <daveu@com1software.com>
2024-01-02 12:42:57 -0500

    update

--- Go/Client Driver/main.go
+++ Go/Client Driver/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	resp, err := http.Get("http://localhost:8080")
+	xip := fmt.Sprintf("http://%s", GetOutboundIP())
+	port := "8080"
+	resp, err := http.Get(xip+":"+port)
 	if err != nil {
 		panic(err)
 	}
@@ -17,11 +21,24 @@ func main() {
 	fmt.Println("Response status:", resp.Status)
 
 	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
+	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
+}
+
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
