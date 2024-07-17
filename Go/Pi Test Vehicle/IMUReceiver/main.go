44a0b7f8ebddf72f04791847cee667d8098b5eb8
Dave <dave@com1software.com>
2024-07-17 15:54:10 -0400

    update

--- Go/Pi Test Vehicle/IMUReceiver/main.go
+++ Go/Pi Test Vehicle/IMUReceiver/main.go
@@ -11,13 +11,21 @@ import (
 	"os/signal"
 	"syscall"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+	ipfile:="ip.cfg"
 	url := "http://com1software.com"
 	url = "http://192.168.1.113:8080"
-	url = FindHost() 
+	lines, _ := readLines(ipfile)
+	data := strings.Join(lines, " ")
+	if len(data)>3{
+		url=data
+	}else{
+	    url = FindHost() 
+	}
 	tc := 0
 	tctl := 0
 	loop := false
@@ -127,7 +135,28 @@ func FindHost() string {
 			x = max
 			fmt.Printf("Host Found at %s\n", url)
 			url = "http://" + url
+			err = os.WriteFile("ip.cfg", []byte(url), 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 	return url
 }
+
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
